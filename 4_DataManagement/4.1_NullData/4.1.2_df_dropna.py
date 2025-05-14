363f7c48a440cd173e4168abe3a455fe3bea9ae8
WOONGKYU JEE <wldndrb1@gmail.com>
2025-05-14 22:42:25 +0900

    dropna basic

---
+++ 4_DataManagement/4.1_NullData/4.1.2_df_dropna.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+
+df =  pd.read_csv('sample.csv',sep=',')
+
+
+print(' * org data --------')
+print(df)
+print(df.keys())
+for key in df.keys():
+	print(key)
+print(df.shape)
+print(df.index)
+for k in df.index:
+	print(k)
+
+print(' * dropping rows including NaN')
+df2 = df.dropna(axis=0)
+print(df2)
+print(df2.values)
+print(df2.values.shape)
+
+print(' * dropping cols including NaN')
+df2 = df.dropna(axis=1)
+print(df2)
+print(df2.values)
+print(df2.values.shape)
+print(df2.values.ravel())
+print(df2.values.ravel().reshape(df2.values.shape))
+
+'''
+	dropna(how='all')
+	dropna(thresh=4)			# if there are non-NaN count less than 4
+
+	dropna(axis=0, subset=['colname']) 			# do dropna (row) for the colname 	# defulat axis=0
+	dropna(axis=1, subset=['rowname' or index])	# do dropna (col) for the rowname or row index
+
+	# > axis=0 w.r.t. row
+	# > axis=1 w.r.t. col
+'''
+
+print(' * renew --------')
+print(df)
+print(df.shape)
+print(df.columns)
+print(df.index)
+#print(df.rows) # wrong !
+print(' * isnull() > returns True/False tells NaN')
+print(df.isnull())
+print(df.isnull().count())
+
+df2 = df.dropna(subset=['C','D'])
+print(df2)
+df2 = df.dropna(subset=[ k for k in df.index ], axis=1) # for all rows
+df2 = df.dropna(subset=df.index, axis=1) # for all rows # this is the same
+print(df2)
+
